48f9527de7aec61b29fdc5ccfe253a05d22f58c6
Kelvin <kelvintorres1612@gmail.com>
2020-12-03 10:37:22 -0400

    DataView update
    DataView now grabs only non-deleted data
    DataView now splits online and manual  surveys

--- src/systemEvaluation/DataView.java
+++ src/systemEvaluation/DataView.java
@@ -5,40 +5,57 @@ import java.io.*;
 
 public class DataView {
 	File file = new File("./instrumentos.csv");
+	File file2 = new File("./temp.csv"); // Holds non-deletable data
 	String data[][]; // Data holder
 	DataView() throws IOException{
-		int sum = -1;
+		int sum = 0;
 		FileReader freader = new FileReader(file);
-		BufferedReader counter = new BufferedReader(freader); // counts observations
+		BufferedReader counter = new BufferedReader(freader); // counts evaluations
 		String test;
-		String splitter[]; // Split lines
+		String splitter[] = null; // Split lines
 		
+		FileWriter writer = new FileWriter(file2);
+		writer.write("header\n");
 		
-		while(counter.readLine() != null) // Count observations
-			sum++;
+		while(counter.readLine() != null) { // Count observations
+			test = counter.readLine(); // Read line
+			splitter = test.split(",",103); // Split line
+			if(splitter[0].equals("FALSE")) { // If not-deletable, add to temp
+				sum++;
+				writer.write(test+"\n");
+			}
+		}
+		
+		writer.close();
 		data = new String[sum][103];
+		System.out.println("Number of evals: "+data.length);
 		
-		FileReader freader2 = new FileReader(file); // Reopen file
+		/////////////////////////////////////////////////////////////////////
+		
+		FileReader freader2 = new FileReader(file2); // Reopen temp file
 		BufferedReader reader = new BufferedReader(freader2);
 		
 		while(reader.readLine() != null) { // Iterate lines
 			for(int i = 0; i < data.length; i++) {
 				test = reader.readLine(); // Read line
+				System.out.println("Test: "+test);
 				splitter = test.split(",",103); // Split line
 				for(int j = 0; j < 103; j++) {
 					data[i][j] = splitter[j]; // Assign values
 				}
 			}
 		}
+		
+		ObSums();
 	}
 	
 	int[] ObSums() {
 		int[] MN = {0,0}; // Online / Local surveys
 		
 		for(int i = 0; i < data.length; i++) {
-			if(data[i][16].equals("Manual")) 
+			if(data[i][16].equals("Manual") && data[i][1].equals("TRUE")) 
 				MN[0]++;
-			else
+			else if(data[i][16].equals("Online") && data[i][1].equals("TRUE"))
 				MN[1]++;
 		}
 		return MN;
