0c1f8d376366191079099b1acdc1e134a12fa6cb
Kelvin <kelvintorres1612@gmail.com>
2020-12-18 10:19:43 -0400

    ValenciasRubrica fully functional

--- src/systemEvaluation/ValenciasCapsule.java
+++ src/systemEvaluation/ValenciasCapsule.java
@@ -14,13 +14,13 @@ public class ValenciasCapsule {
 	// g) Disponibilidad y eficacia en la orientaci�n acad�mica del estudiante.
 	// h) Posesi�n de los atributos de integridad, laboriosidad, liberalidad y objetividad en la ense�anza
 	
-	protected int valen1A;
-	protected int valen1B; 
-	protected int valen1C; 
-	protected int valen1D;
-	protected int valen1E; 
-	protected int valen1F; 
-	protected int valen1GH;
+	protected double valen1A;
+	protected double valen1B; 
+	protected double valen1C; 
+	protected double valen1D;
+	protected double valen1E; 
+	protected double valen1F; 
+	protected double valen1GH;
 	
 	// Servicio a la Institucion
 	// a) Trabajo en comit�s de facultad a nivel departamental.
@@ -31,16 +31,16 @@ public class ValenciasCapsule {
 	// f) Designaci�n como director/a de departamento, presidente de comit� y otras.
 	// g) Participaci�n en organismos de gobierno; tales como: el Senado y el Consejo Universitario.
 	
-	protected int valen2A;
-	protected int valen2BC; 
-	protected int valen2DE;
-	protected int valen2FG;
+	protected double valen2A;
+	protected double valen2BC; 
+	protected double valen2DE;
+	protected double valen2FG;
 	
 	// Servicio a la comunidad
 	// a) Servicio en el campo profesional del profesor como consultor o investigador.
 	// b) Servicio como recurso: conferenciante de grupos 2 de la comunidad, participaci�n activa en gestiones pol�ticas, religiosas o c�vicas.
 	
-	protected int valen3AB;
+	protected double valen3AB;
 	
 	// Investigacion y Trabajo Creativo
 	// a) Publicaciones
@@ -49,9 +49,9 @@ public class ValenciasCapsule {
 	// d) Concesi�n de ayudas para investigaci�n y proyectos.
 	// e) Invenciones, patentes, labor art�stica y actuaci�n.
 	
-	protected int valen4A; 
-	protected int valen4B; 
-	protected int valen4CDE;
+	protected double valen4A; 
+	protected double valen4B; 
+	protected double valen4CDE;
 	
 	// Crecimiento y Desarrollo profesional
 	// a) Obtenci�n de premios, ayudas y becas.
@@ -61,7 +61,7 @@ public class ValenciasCapsule {
 	// e) Participaci�n en conferencias, congresos o institutos.
 	// f) Educaci�n post doctoral, educaci�n continua.
 	
-	protected int valen5ABC;
-	protected int valen5D;
-	protected int valen5EF;
+	protected double valen5ABC;
+	protected double valen5D;
+	protected double valen5EF;
 }
