9224ba093ef1a96294c0273fb69941e60c9e1b6b
Yareth Melendez <yareth.rivera2000@gmail.com>
2020-12-20 16:25:55 -0400

    minimal changes

--- src/systemEvaluation/ContinuarEvaluacion.java
+++ src/systemEvaluation/ContinuarEvaluacion.java
@@ -15,6 +15,7 @@ import java.util.Scanner;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
 public class ContinuarEvaluacion extends JFrame {
 
@@ -26,7 +27,7 @@ public class ContinuarEvaluacion extends JFrame {
 	LogIn user = new LogIn();
 	String User = user.getID();
 	static String IDEBox;
-	
+
 	public ContinuarEvaluacion() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(-7, 0, 1920, 1080);
@@ -35,10 +36,15 @@ public class ContinuarEvaluacion extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
+		JLabel lblSelecionaUnaEvaluacion = new JLabel("Seleciona una Evaluacion para Continuar");
+		lblSelecionaUnaEvaluacion.setBounds(643, 66, 1590, 74);
+		lblSelecionaUnaEvaluacion.setFont(new Font("Tahoma", Font.BOLD, 30));
+		contentPane.add(lblSelecionaUnaEvaluacion);
+
 
 
 		btnNewButton = new JButton("Retroceder ");
-		btnNewButton.setBounds(1690, 44, 145, 46);
+		btnNewButton.setBounds(1081, 901, 145, 46);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 
@@ -55,8 +61,8 @@ public class ContinuarEvaluacion extends JFrame {
 		JButton btnCargar = new JButton("Cargar");
 		btnCargar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				
+
+
 				IDEBox = textField.getText();
 				String[] ag = new String[164] ;
 
@@ -65,9 +71,9 @@ public class ContinuarEvaluacion extends JFrame {
 				String[] ops;
 				File instru;
 				int size = 0;
-				
-			
-				
+
+
+
 				try {
 					instru = new File("./instrumentos.csv");
 					Scanner scanner = new Scanner(instru);
@@ -86,69 +92,76 @@ public class ContinuarEvaluacion extends JFrame {
 					System.out.print("404 "+ ex);
 				}
 
-					onlineEvaluation u ;
-			
+				onlineEvaluation u ;
+
 				Evaluation i ;
 				try {
-					 i = new Evaluation();
+					i = new Evaluation();
 					u = new onlineEvaluation();
-					 for(int h = 0; h <size ;h++) {
+					for(int h = 0; h <size ;h++) {
 
-							fileManager continuar = new fileManager();
-							ag = continuar.combobox(User,h);
-							System.out.print(ag[12]+ "owowowowowowowowowowowowo");
-							
+						fileManager continuar = new fileManager();
+						ag = continuar.combobox(User,h);
+						System.out.print(ag[12]+ "owowowowowowowowowowowowo");
 
-							if ( Integer.parseInt(IDEBox) > 0 && IDEBox.trim().equals(ag[12])) {
-								
-								if(ag[13].equals("Online")) {
+
+						if ( Integer.parseInt(IDEBox) > 0 && IDEBox.trim().equals(ag[12])) {
+
+							if(ag[13].equals("Online")) {
 								u.comboboxItems(User, Integer.parseInt(IDEBox));
 								u.setVisible(true);
 								dispose();
-								}
-								
-								if(ag[13].equals("Manual")) {
+							}
+
+							if(ag[13].equals("Manual")) {
 								i.comboboxItems(User, Integer.parseInt(IDEBox));
 								i.setVisible(true);
 								dispose();
-								}
-								}
+							}
 						}
+					}
 				} catch (IOException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
+<<<<<<< HEAD
+
+
+
+
+
+
+
+=======
 				
 				
+>>>>>>> 9fa44a4a47d24178af2a001a88cc82f7ed379685
 			}
 		});
-		btnCargar.setBounds(871, 955, 145, 46);
+		btnCargar.setBounds(745, 901, 145, 46);
 		btnCargar.setFont(new Font("Tahoma", Font.BOLD, 15));
 		contentPane.add(btnCargar);
-		
-		
+
+
 
 		textField = new JTextField();
-		textField.setBounds(877, 898, 124, 46);
+		textField.setBounds(581, 901, 124, 46);
 		contentPane.add(textField);
 		textField.setColumns(10);
 
-		JLabel lblNewLabel = new JLabel("Escoge el numero de evaluacion para continuar:");
-		lblNewLabel.setBounds(792, 863, 311, 25);
+		JLabel lblNewLabel = new JLabel("Escoge el numero de evaluacion");
+		lblNewLabel.setBounds(540, 872, 213, 25);
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
 		contentPane.add(lblNewLabel);
-		
-				JLabel lblSelecionaUnaEvaluacion = new JLabel("Seleciona una Evaluacion para Continuar");
-				lblSelecionaUnaEvaluacion.setBounds(676, 66, 777, 74);
-				lblSelecionaUnaEvaluacion.setFont(new Font("Tahoma", Font.BOLD, 30));
-				contentPane.add(lblSelecionaUnaEvaluacion);
-				
-				JScrollPane scrollPane = new JScrollPane();
-				scrollPane.setBounds(0, 0, 1919, 1036);
-				contentPane.add(scrollPane);
+
+		JPanel panel = new JPanel();
+		panel.setBounds(455, 121, 945, 713);
+		contentPane.add(panel);
+		panel.setLayout(null);
+
 
 		String[] ag = new String[160];
-		
+
 		int j = 40;
 
 		String[] ops;
@@ -179,27 +192,23 @@ public class ContinuarEvaluacion extends JFrame {
 			fileManager continuar = new fileManager();
 			ag = continuar.combobox(User,i);
 
-			System.out.println("ID DE Evaluacion"+ag[12]+" "+ag[1]+" "+ag[13]);
+			System.out.println("Numero de evaluacion: "+ag[12]+" \tProfesor evaluado: "+ag[1]+" \tModalidad: "+ag[13]+ " \tPeriodo: "+ag[6]);
 
 			if(ag[1] != null) {
-				textField_1 = new JTextField("ID DE Evaluacion"+ag[12]+" "+ag[1]+" "+ag[13]);
+				textField_1 = new JTextField("    Numero de evaluacion: "+ag[12]+" \tProfesor evaluado: "+ag[1]+" \tModalidad: "+ag[13]+ " \tPeriodo: "+ag[6]);
 				textField_1.setEditable(false);
-				textField_1.setBounds(667, j +177, 594, 39);
-				contentPane.add(textField_1);
+				textField_1.setBounds(100, j + 5, 933, 26);
+				panel.add(textField_1);
 				textField_1.setColumns(10);
-				j = j+40;
+				j = j+30;
+
 			}
 
-
-		}
-
+		}	
 
 	}
 	public String getIDEBox() {
 		return IDEBox;
-		
+
 	}
 }
-
-
-
