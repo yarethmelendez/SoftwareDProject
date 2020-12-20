a69425b25f3623ff3cb3b3331dfe0bddcb9ba2fa
ana2485 <nataliasierra591@gmail.com>
2020-12-19 22:13:22 -0400

    final changes

--- src/systemEvaluation/ComentariosOnline.java
+++ src/systemEvaluation/ComentariosOnline.java
@@ -36,7 +36,7 @@ public class ComentariosOnline extends JFrame {
 	 */
 	public ComentariosOnline() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(0, 0, 1080, 2090);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
--- src/systemEvaluation/Evaluation.java
+++ src/systemEvaluation/Evaluation.java
@@ -4290,7 +4290,13 @@ public class Evaluation extends JFrame  {
 		i2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
-				Comentarios e = new Comentarios();
+				Comentarios e = null;
+				try {
+					e = new Comentarios();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				e.setVisible(true)
 ;			}
 		});
